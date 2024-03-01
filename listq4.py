2d777e018bd280492f63f2ae7ead6f3f8476de47
JIBIN JACOB <105502151+jj1533@users.noreply.github.com>
2024-03-01 06:27:07 +0530

    Delete listq4.py

--- listq4.py
+++
@@ -1,9 +0,0 @@
-numlist=input("Enter numbers: ")
-numbers=numlist.split()
-a=[]
-numbers=[int(num) for num in numbers]
-for i in range(len(numbers)):
-    if numbers[i]%2==0:
-            a.append(numbers[i])
-            a.sort()
-print("Even Numbers: ",a)
